fa9702624929bf9c8b14dd28765fd3d999ef417c
Katarzyna Śliwka <sliwkakatarzyna12@gmail.com>
2023-06-22 22:42:13 +0200

    changing display position of the selectedBook

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,16 +1,37 @@
+import classes from "./BooksList.module.css";
+import { Row, Col } from "react-bootstrap";
 
 const Books = ({ book, isSelected, onRowClick }) => {
-
   const handleRowClick = () => {
     onRowClick(book);
   };
 
   return (
-    <tr className={isSelected ? "selected" : ""} onClick={handleRowClick}>
-      <td>{book.id}</td>
-      <td>{book.title}</td>
-      <td>{book.authors}</td>
-    </tr>
+    <>
+      <tr
+        className={`${isSelected ? "selected" : ""} ${classes.row}`}
+        onClick={handleRowClick}
+      >
+        <td>{book.id}</td>
+        <td>{book.title}</td>
+        <td>{book.authors}</td>
+      </tr>
+      {isSelected && (
+        <tr className={`${classes.row} ${classes.selectedDetails}`}>
+          <td colSpan={3}>
+            <div>
+              <h2>Selected Book Details</h2>
+              <p>ID: {book.id}</p>
+              <p>Title: {book.title}</p>
+              <p>Authors: {book.authors}</p>
+              <p>Self Link: {book.selfLink}</p>
+              <p>Page Count: {book.pageCount}</p>
+              <img src={book.imageLinks} alt="Book Cover" />
+            </div>
+          </td>
+        </tr>
+      )}
+    </>
   );
 };
 
--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,6 +1,7 @@
 import Books from "./Books";
 import { useState } from "react";
 import Breadcrumb from "./Breadcrumb";
+import { Container } from "react-bootstrap";
 
 const BooksList = ({ books }) => {
   const [selectedBook, setSelectedBook] = useState(null);
@@ -27,42 +28,33 @@ const BooksList = ({ books }) => {
   };
 
   return (
-    <div>
-      <Breadcrumb
-        path={selectedPath}
-        onBreadcrumbClick={handleBreadcrumbClick}
-      />
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Authors</th>
-          </tr>
-        </thead>
-        <tbody>
-          {books.map((book, index) => (
-            <Books
-              key={book.id}
-              book={book}
-              isSelected={selectedBook === book}
-              onRowClick={handleRowClick}
-            />
-          ))}
-        </tbody>
-      </table>
-      {selectedBook && (
-        <div>
-          <h2>Selected Book Details</h2>
-          <p>ID: {selectedBook.id}</p>
-          <p>Title: {selectedBook.title}</p>
-          <p>Authors: {selectedBook.authors}</p>
-          <p>Self Link: {selectedBook.selfLink}</p>
-          <p>Page Count: {selectedBook.pageCount}</p>
-          <img src={selectedBook.imageLinks}></img>
-        </div>
-      )}
-    </div>
+    <Container>
+      <div>
+        <Breadcrumb
+          path={selectedPath}
+          onBreadcrumbClick={handleBreadcrumbClick}
+        />
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Authors</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, index) => (
+              <Books
+                key={book.id}
+                book={book}
+                isSelected={selectedBook === book}
+                onRowClick={handleRowClick}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Container>
   );
 };
 
